f0a761367180c4b3d981bc86f66c1e66d56c91e1
Mahsa Saeedy <45897831+SMSaeedi@users.noreply.github.com>
2022-01-29 16:03:36 +0330

    Add files via upload

--- FindFirstUniqueCharacter.java
+++ FindFirstUniqueCharacter.java
@@ -3,14 +3,14 @@ package com.example.demo;
 public class FindFirstUniqueCharacter {
     static String str = "efficient";
     static String str1 = "mahsa";
+    static String str2 = "ssff";
+    static String str3 = "";
 
     public static void main(String[] args) {
-        try {
-            System.out.println(findFirstUniqueCharacter(str));
-            System.out.println(findFirstUniqueCharacter(str1));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println(findFirstUniqueCharacter(str));
+        System.out.println(findFirstUniqueCharacter(str1));
+        System.out.println(findFirstUniqueCharacter(str2));
+        System.out.println(findFirstUniqueCharacter(str3));
     }
 
     /*
@@ -18,25 +18,30 @@ public class FindFirstUniqueCharacter {
      * Example: efficient -> c
      */
     private static Character findFirstUniqueCharacter(String input) {
-        String str = null;
-        char[] outErr = null;
+        String str;
+        boolean uniqueCount = false;
         Character outPut = null;
+
         if (input.length() == 0 || input.equals(null)) {
             str = "the string is empty";
-            outErr = str.toCharArray();
-            System.out.println(outErr);
+            System.out.println(str);
+            return Character.valueOf(str.toCharArray()[3]);
         }
 
         for (Character ch : input.toCharArray()) {
             if (input.indexOf(ch) == input.lastIndexOf(ch)) {
                 outPut = ch;
+                uniqueCount = true;
                 return outPut;
-            } else {
-                str = "no pair char";
-                outErr = str.toCharArray();
-                System.out.println(outErr);
             }
         }
-        return outPut;
+
+        if (!uniqueCount) {
+            str = "no first pair char";
+            System.out.println(str);
+            return Character.valueOf(str.toCharArray()[2]);
+        }
+
+        return null;
     }
 }
